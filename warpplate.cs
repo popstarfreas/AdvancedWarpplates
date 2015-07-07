015c4e660dceed265d7280e37b880a354f2fa2db
Zaicon <kzaicon@yahoo.com>
2015-07-07 01:50:58 -0500

    TShock 4.3.1Pre Update

--- warpplate.cs
+++ warpplate.cs
@@ -7,7 +7,7 @@ using TShockAPI;
 
 namespace PluginTemplate
 {
-    [ApiVersion(1, 17)]
+    [ApiVersion(1, 18)]
     public class WarpplatePlugin : TerrariaPlugin
     {
         public static List<Player> Players = new List<Player>();
